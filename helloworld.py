2ec2ac0e21ce77a4ae7b3ea045a57a29f05e8f85
alangonzalez87 <alan.gonzalez87@hotmail.com>
2023-01-06 18:16:37 -0300

    first commit

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+color=  input ('please type your favorite color: ')
+print('you choice... ')
+print(color )
+age= int (input ('what is your age?'))
+print(age)
+if age >=20 :
+    print('you are of legal age')
+elif age <= 19:
+    print('your are underage')
