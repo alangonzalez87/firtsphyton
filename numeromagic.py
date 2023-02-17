31f6ef4ab09a9d9ab2cc7238a51b8b5415efeebb
Alan Gonzalez <73410711+alangonzalez87@users.noreply.github.com>
2023-02-17 07:46:36 -0300

    Add files via upload

---
+++ numeromagic.py
@@ -0,0 +1,31 @@
+import random
+
+
+
+invitado= 'yes'
+
+while invitado == 'yes':
+    number_magico = random.randint(1,10)
+    
+    intentos=0
+
+    intentos_inv = -1
+
+    while intentos_inv != number_magico:
+        intentos_inv=int (input('puedes adivinar el numero magico?\n'))
+       
+        intentos = intentos +1
+       
+        if intentos_inv < number_magico:
+            print('mas alto')
+        elif intentos_inv > number_magico:
+            print ('mas bajo')
+        else:
+            print('adivinaste!')
+    print(f'te costo: {intentos}')
+    invitado= input('queres jugar de nuevo?\n yes/no: ')
+
+
+print('see you later my friends!')
+
+
